445b829a4bde69adfb66b670e1af03d9f7dc4afc
dopswae98 <ephraimmatarutse@gmail.com>
2023-08-11 00:13:04 +0200

    solved routing now now

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
   };
 
   useEffect(() => {
-    setFakeAuthService({ ...fakeAuthService, isAuthenticated: false });
+    // setFakeAuthService({ ...fakeAuthService, isAuthenticated: false });
   }, []);
 
   const [isLoading, setIsLoading] = useState(false);
